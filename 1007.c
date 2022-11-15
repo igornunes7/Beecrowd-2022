1afa603f43cf44566b8083e39de3cb0159654c50
Igor Nunes <101185370+igornunes7@users.noreply.github.com>
2022-11-15 13:56:26 -0400

    Create 1007.c

---
+++ 1007.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ int a,b,c,d, dif;
+ 
+ scanf ("%d" , &a);
+ scanf ("%d" , &b);
+ scanf ("%d" , &c);
+ scanf ("%d" , &d);
+ 
+ dif = (a * b) - (c * d);
+ 
+ printf ("DIFERENCA = %d\n" , dif);
+ 
+    return 0;
+}
