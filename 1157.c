8eb471857d845a9155bcfd30531ea9072b5ade3d
Igor Nunes <101185370+igornunes7@users.noreply.github.com>
2022-11-15 13:37:34 -0400

    Create 1157.c

---
+++ 1157.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+    int N;
+    int i;
+    
+    scanf ("%d" , &N);
+    
+    for (i = 1; i <= N; i++){
+        if (N % i == 0) {
+           printf ("%d\n" , i); 
+        }
+    }
+ 
+    return 0;
+}
