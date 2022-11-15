468fc98010865d16e34af940e3d6ab4357816138
Igor Nunes <101185370+igornunes7@users.noreply.github.com>
2022-11-15 13:55:35 -0400

    Create 1004.c

---
+++ 1004.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ int a, b, produto;
+ 
+ scanf ("%d" , &a);
+ scanf ("%d" , &b);
+ 
+ produto = a * b;
+ 
+ printf ("PROD = %d\n" , produto);
+ 
+    return 0;
+}
